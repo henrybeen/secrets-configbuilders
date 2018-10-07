051c4f0117e1ebefdad246f68383850acb67fb14
Henry Been <consultancy@henrybeen.nl>
2018-10-07 20:23:11 +0200

    git commit -m "working example of a combination of user secrets and azure key vault for loading secrets using config builders"

---
+++ src/Demo.ConfigBuilders.Application/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Demo.KeyVaultManagedIdentity.Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            ViewBag.Secret = ConfigurationManager.AppSettings["Secret"];
+
+            return View();
+        }
+    }
+}
